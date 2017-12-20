439981a8e149fdb9ffad2f4b397a7a1b08b19f40
pop1234o <859405648@qq.com>
2017-12-20 11:11:02 +0800

    添加了几种设计模式

---
+++ Algorithm/src/main/java/com/liyafeng/designpattern/Main.java
@@ -0,0 +1,18 @@
+package com.liyafeng.designpattern;
+
+/**
+ * Created by lenovo on 2017/12/20.
+ */
+
+public class Main {
+
+    /**
+     * 设计模式的网站
+     * http://design-patterns.readthedocs.io/zh_CN/latest/creational_patterns/creational.html
+     * 这个网站讲解的很详细，值得收藏
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
