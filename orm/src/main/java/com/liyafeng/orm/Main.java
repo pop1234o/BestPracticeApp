b24921ab1e3362ea373415d8312546e09c6aba74
pop1234o <859405648@qq.com>
2018-01-02 10:38:45 +0800

    添加了greendao的使用

---
+++ orm/src/main/java/com/liyafeng/orm/Main.java
@@ -0,0 +1,25 @@
+package com.liyafeng.orm;
+
+/**
+ * Created by liyafeng on 2018/1/2.
+ */
+
+public class Main {
+
+    /**
+     * @param args
+     * 我们在gradle添加 编译插件
+     * 然后添加version声明，这个会自动生成代码中体现
+     * 然后定义一个Bean，然后加上@Entity注解
+     * build-》make project，这样就在build-》generated-》source-》greendao中
+     * 生成DaoMaster，DaoSession，XXXDao，这几个文件
+     * 我们要自己定义 MyOpenHelper继承DaoMaster.OpenHelper ，复写onUpgrade()
+     * 然后我们在Application中新建 MyOpenHelper对象，getWritableDb()
+     * new DaoMaster(db).newSession();然后我们将DaoSession写个getter
+     * 接下来就可以使用了
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ viewapp/src/main/java/com/liyafeng/view/fragment/Main.java
@@ -0,0 +1,28 @@
+package com.liyafeng.view.fragment;
+
+import android.content.Context;
+
+/**
+ * Created by liyafeng on 2017/12/29.
+ */
+
+public class Main {
+
+    /**
+     * @param args
+     * https://developer.android.google.cn/guide/components/fragments.html
+     * 我们要用support-v4包下的FragmentManager 和Fragment
+     * 然后 开启事务
+     * add ,是将一个 片段 加入到布局中
+     * 而replace是将布局中存在的一个Fragment替换为这个Fragment，如果没有，则无效果
+     *
+     * Fragment也可以后退，像Activity一样也有回退栈
+     * -----------------------
+     * Fragment本身就是一个View，只不过用FragmentActivity，和FragmentManager来赋予它生命周期
+     * 这样就是包装了一层，使得我们可以复用代码
+     *
+     *
+     */
+    public static void main(String[] args) {
+    }
+}
