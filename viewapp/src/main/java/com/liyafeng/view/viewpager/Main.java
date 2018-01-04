9beed5c7d0023a3382aaaf19c649a00324bcde34
pop1234o <859405648@qq.com>
2018-01-04 15:31:16 +0800

    添加了viewpager的注意点

---
+++ viewapp/src/main/java/com/liyafeng/view/viewpager/Main.java
@@ -0,0 +1,16 @@
+package com.liyafeng.view.viewpager;
+
+/**
+ * Created by liyafeng on 2018/1/4.
+ */
+
+public class Main {
+
+    /**
+     * @param args
+     * 注意在Adapter中记得container.addView，否则显示不出来
+     */
+    public static void main(String[] args) {
+
+    }
+}
--- viewapp/src/main/java/com/liyafeng/view/viewpagerindicator/Main.java
+++ viewapp/src/main/java/com/liyafeng/view/viewpagerindicator/Main.java
@@ -9,11 +9,13 @@ public class Main {
     /**
      * 我们可以用j神的viewpagerIndicator
      *
-     * 当然还可以用support-v4中的PagerTabStrip
+     * 当然还可以用support-v4中的PagerTabStrip (这个效果太差了，title还会滑动)
      * https://developer.android.google.cn/reference/android/support/v4/view/PagerTabStrip.html
      *
      * 还有support-design中的TabLayout
      * https://developer.android.google.cn/reference/android/support/design/widget/TabLayout.html
+     *  compile 'com.android.support:design:25.0.0'
+     *
      *
      * @param args
      */
