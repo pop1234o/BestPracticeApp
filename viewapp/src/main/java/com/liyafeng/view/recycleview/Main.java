1e50102baf7723243e39a22e1ad5658087655c75
pop1234o <859405648@qq.com>
2018-01-08 17:59:55 +0800

    添加了recyclerView的注意事项

---
+++ viewapp/src/main/java/com/liyafeng/view/recycleview/Main.java
@@ -0,0 +1,17 @@
+package com.liyafeng.view.recycleview;
+
+/**
+ * Created by liyafeng on 2018/1/8.
+ */
+
+public class Main {
+
+    /**
+     * 填充的item如果要用xml中的参数，就指定parent，但是不attachToRoot
+     * inflater.inflate(R.layout.xxx, parent, false)
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
