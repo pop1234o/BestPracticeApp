1c41a19aef910e618b64ed66080f941593d6ae4e
pop1234o <859405648@qq.com>
2018-01-09 10:09:53 +0800

    添加了对MVP的理解

---
+++ architecture/src/main/java/com/liyafeng/architecture/mvp/Main.java
@@ -0,0 +1,20 @@
+package com.liyafeng.architecture.mvp;
+
+/**
+ * Created by liyafeng on 2018/1/9.
+ */
+
+public class Main {
+    /**
+     * @param args
+     * mvp真正实现了业务逻辑和 UI逻辑的解耦
+     * 与UI无关的都在presenter中，业务表现层
+     * UI显示的都用接口方式回调
+     * ----------------
+     *
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+}
