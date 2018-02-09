df5fa9a3d6fa4e82ff9ad3452f9e2a7f140baef7
pop1234o <859405648@qq.com>
2018-02-09 18:29:07 +0800

    添加了算法题 丑数

---
+++ Algorithm/src/main/java/com/liyafeng/algorithm/N33_PrintMinNumber.java
@@ -0,0 +1,22 @@
+package com.liyafeng.algorithm;
+
+/**
+ * Created by liyafeng on 2018/1/31.
+ */
+
+public class N33_PrintMinNumber {
+
+
+    /**
+     * @param args
+     * 给定一组数组，输出组成所有数字中最小的那个
+     * 比如{3,32,321},输出的最小数字是 321323
+     *
+     * ------------------------
+     * 一个新的排序规则来排序，mn < nm 那么m< n
+     * 依次类推，排好序的组合就是最小值
+     */
+    public static void main(String[] args) {
+
+    }
+}
