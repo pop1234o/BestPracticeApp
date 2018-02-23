1fa84413ce99b0386284f8c03ff8fc4307d427a8
pop1234o <859405648@qq.com>
2018-02-23 14:36:28 +0800

    添加了冒泡排序和基数排序

---
+++ Algorithm/src/main/java/com/liyafeng/algorithm/sort/N_Sort_BubbleSort.java
@@ -0,0 +1,34 @@
+package com.liyafeng.algorithm.sort;
+
+/**
+ * Created by liyafeng on 2018/2/23.
+ */
+
+public class N_Sort_BubbleSort {
+
+
+    /**
+     * 冒泡排序
+     * 时间复杂度：O(n^2)
+     * =======原理===========
+     * 每次把最大的元素冒泡到数组的最右端
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] array = {1, 124, 54, 34, 34, 6, 5};
+        sort(array);
+        Util.print(array);
+    }
+
+    private static void sort(int[] array) {
+        int length = array.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (Util.less(array, j + 1, j)) {
+                    Util.exchange(array, j, j + 1);
+                }
+            }
+        }
+    }
+}
---
+++ Algorithm/src/main/java/com/liyafeng/algorithm/sort/N_Sort_RadixSort.java
@@ -0,0 +1,19 @@
+package com.liyafeng.algorithm.sort;
+
+/**
+ * Created by liyafeng on 2018/2/23.
+ */
+
+public class N_Sort_RadixSort {
+
+    /**
+     * 基数排序
+     * ============原理===========
+     * 将所有数字变为统一长度，不足补0,然后从个位开始比较，放入容器
+     * 然后取出，然后取十位，放入容器
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
