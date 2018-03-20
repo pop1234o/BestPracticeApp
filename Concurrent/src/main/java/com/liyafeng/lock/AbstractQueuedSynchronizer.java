f6b10048f15013e90f44440606da6f9c96d0603e
pop1234o <859405648@qq.com>
2018-03-20 18:57:59 +0800

    添加了锁代码

--- Concurrent/src/main/java/com/liyafeng/lock/AbstractQueuedSynchronizer.java
+++ Concurrent/src/main/java/com/liyafeng/lock/AbstractQueuedSynchronizer.java
@@ -5,6 +5,33 @@ package com.liyafeng.lock;
  */
 
 public abstract class AbstractQueuedSynchronizer extends AbstractOwnableSynchronizer {
+    private static final long serialVersionUID = 7373984972572414691L;
+
+
+    /**
+     * 只能在内部类 的外部类中初始化
+     */
+    protected AbstractQueuedSynchronizer() { }
+
+
+    static final class Node{
+
+    }
+
+    private transient volatile Node head;
+
+    private transient volatile Node tail;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    private volatile int state;
+
 
 
 }
