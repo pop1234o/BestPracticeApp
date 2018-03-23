03f7119aad3a58ead69c398009c8d8a2c9adc654
pop1234o <859405648@qq.com>
2018-03-23 14:37:30 +0800

    完善了并发

--- Concurrent/src/main/java/com/liyafeng/lock/Condition.java
+++ Concurrent/src/main/java/com/liyafeng/lock/Condition.java
@@ -5,6 +5,16 @@ package com.liyafeng.lock;
  *
  * 用来代替 Object中的wait(),notify(), notifyAll()
  * 可以指定唤醒某个线程，比Object更灵活
+ * 他要在Lock子类的lock和unlock间使用
+ * 可以参考{@link java.util.concurrent.locks.AbstractQueuedSynchronizer}
+ * 中的ConditionObject。
+ * {@link java.util.concurrent.locks.ReentrantLock}
+ * 中有使用方法
+ *
+ * {@link java.util.concurrent.locks.Condition}
+ * ====================
+ * Condition 是AQS的内部类，所以一个Condition对应一个AQS
+ *
  */
 
 public class Condition {
