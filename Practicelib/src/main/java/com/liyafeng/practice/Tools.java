5ace64ca1e278bdcc8755017bcb9f5332de4ef54
pop1234o <859405648@qq.com>
2018-04-07 11:59:09 +0800

    添加了apk反编译

--- Practicelib/src/main/java/com/liyafeng/practice/Tools.java
+++ Practicelib/src/main/java/com/liyafeng/practice/Tools.java
@@ -42,8 +42,8 @@ public class Tools {
      * https://joyrun.github.io/2016/07/19/AptHelloWorld/
      * https://www.jianshu.com/p/2494825183c5
      * https://juejin.im/entry/584a29a5128fe1006c7923a5
-     * */
-    public void a1_2(){
+     */
+    public void a1_2() {
         /*
         * 最早我们用android-apt 这个工具，但是现在已经维护了
         * 因为Gradle推出了官方的处理工具 annotationProcessor
@@ -78,8 +78,8 @@ public class Tools {
      * https://blog.csdn.net/jiangwei0910410003/article/details/50402000（signapk.jar源码分析）
      * https://juejin.im/entry/575ed0bb1532bc00609c3aa9
      * https://maoao530.github.io/2017/01/31/apk-sign/
-     * */
-    public void a2(){
+     */
+    public void a2(Context context) {
         /*
         * 整个过程用到了SHA-1 的hash算法，和RSA非对称加密
         * 首先签名过程就是在apk(压缩包)中生成一个META-INF文件夹，里面有三个文件
@@ -90,9 +90,51 @@ public class Tools {
         * keytool -genkeypair -v -keyalg DSA -keysize 1024 -sigalg SHA1withDSA -validity 20000 -keystore D:\xxx.keystore -alias 别名 -keypass 使用密码 -storepass 打包密码？
         * apksigner 签名时用的是pk8和x509.pem文件，xxx.keystore文件可以转化为他们
         * apksigner.jar 源码分析 源码位置：com/android/signapk/sign.java
-        * 1，
-        * 2
-        * 3
+        * 1，首先生成MANIFEST.MF 文件，apksigner.jar对工程中所有文件的内容做Sha-1计算，
+        *   生成摘要信息，然后对摘要进行Base64编码，然后将 name:[文件名] sha1-digest:[摘要]
+        *   这个键值对写入文件中。（这样能保证每个文件内容没有被修改过，因为修改了sha1值会变）
+        * 2,生成CERT.SF，对MANIFEST.MF 文件做sha1生成摘要信息，然后base64编码
+        *   将sha1-digest-manifest:[摘要]写入文件首部
+        *   对MANIFEST.MF中的每个“键值对”做sha-1，然后base64编码，写入 name:[key] sha1-digest:[摘要]
+         *  （这样就能保证MANIFEST.MF 中的“键值对”没有被修改过）
+        * 3，生成CERT.RSA，里面存储了签名使用的算法，公钥信息，然后将CERT.SF和前面的信息用私钥加密
+        *   然后写入文件结尾（这样就保证了前面所以文件都不能被修改，因为用公钥解密这个密文，得出
+        *   的结果要和之前的信息一致）
         */
+
+        context.getResources().getDrawable(R.drawable.CERT_RSA);
+    }
+
+    /**
+     * apk反编译的流程？
+     * x.509是什么？
+     */
+    public void a3(Context context) {
+        /*
+        * ===================反编译查看内容=============================
+        * apktool 下载 https://ibotpeaches.github.io/Apktool/install/
+        *   反编译出的资源，能查看出Manifest的内容，还有string ,value，layout中的文本
+        *   正常解压出来查看是乱码
+        *   apk d[ecode] filename
+        * dex2jar 下载 https://sourceforge.net/projects/dex2jar/files/
+        *    d2j-dex2jar.bat filename.dex 转化为jar包
+        * jd-gui http://jd.benow.ca/
+        *    点击jd-gui.exe 开启程序，打开上面转化后的jar包，然后就可以查看代码了
+        * ===========================从新打包=============================
+        * 使用 apktool b[uild] -o new.apk <反编译后的文件夹名称>
+        * 这样就生成了 new.apk，但是这种没有 META-INF文件夹，安装会提示失败
+        * 因为这个没有签名。
+        * 所以我们对它签名，先要生成签名文件，xx.keystore,用keytool这个工具，这个是jdk中的
+        * keytool -genkey -alias demo.keystore -keyalg RSA -validity 40000 -keystore demo.keystore
+        * 生成了demo.keystore ，然后我们要用jarsigner.exe(也是jdk中的)对apk进行签名
+        * jarsigner -verbose -keystore demo.keystore new.apk demo.keystore
+        * 这样就在apk中生成了MATE-INF文件夹
+        * 然后我们就可以安装了，但是要把之前安装的程序删除，因为我们的签名文件不一样
+        * 否则会提示更新冲突，和之前的签名文件不一致，导致安装失败
+        *
+        * ====================x.509是什么？======================
+        * 是一种证书的格式，规定了公钥name:value的一些格式
+        */
+        context.getResources().getDrawable(R.drawable.CERT_RSA);
     }
 }
