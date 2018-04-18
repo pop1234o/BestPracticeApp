276ec571517594365562cf859328189831ad443f
pop1234o <859405648@qq.com>
2018-04-18 18:12:15 +0800

    最近忙着公司的项目，提交的少了

---
+++ JNI/src/main/java/com/liyafeng/jni/Hello.java
@@ -0,0 +1,10 @@
+package com.liyafeng.jni;
+
+/**
+ * Created by liyafeng on 2018/4/18.
+ */
+
+public class Hello {
+
+    public static native String doSomething(String s);
+}
---
+++ JNI/src/main/jni/Hello.cpp
@@ -0,0 +1,7 @@
+//
+// Created by lenovo on 2018/4/18.
+//
+
+jstring Java_com_liyafeng_jni_Hello_doSomething(JNIEnv *env){
+
+}
