90cb6663a630049db6a6a4fb06c6c12126d413ae
pop1234o <859405648@qq.com>
2018-06-05 17:35:24 +0800

    添加了知识

--- Event/src/main/java/com/liyafeng/event/rxjava/Main.java
+++ Event/src/main/java/com/liyafeng/event/rxjava/Main.java
@@ -30,6 +30,25 @@ public class Main {
      *
      * 一般我们返回一个数据源，然后我们用流式处理他
      *
+     *
+     * ===================rxjava结合mvp=====
+     * http://wuxiaolong.me/2016/06/12/mvpRetrofitRxjava/
+     *
+     * 网络层返回一个Observable<T>对象 ，在Presenter中处理它，然后数据流处理完，交给view层来更新
+     *
+     *
+     * https://www.jianshu.com/p/a7e58fab4ff1
+     *
+     * 将传统的model层回调，变为model层直接返回Observable对象，这样就省去了写回调方法
+     *
+     * 但是有可能我们获得model层的数据后需要在model层做统一处理。。。
+     *
+     * 我们也可以封装一层callback，如果有统一处理就在model中callback,然后在present中传入callback
+     * 如果没有统一处理，那么直接用presenter传入的callback
+     *
+     *
+     *
+     *
      * @param args
      */
     public static void main(String[] args) {
