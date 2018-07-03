a9bf771b1fb0d6ed81439f61a88a15597c913b7f
pop1234o <859405648@qq.com>
2018-07-03 15:27:10 +0800

    添加了basepageradapter

---
+++ ViewApp/src/main/java/com/liyafeng/view/other/pay/Main.java
@@ -0,0 +1,27 @@
+package com.liyafeng.view.other.pay;
+
+public class Main {
+
+
+    /**
+     * http://wuxiaolong.me/2016/11/22/AndroidPay/
+     * <p>
+     * 微信支付，支付宝支付
+     * 先请求服务器接口，生成预支付订单，服务端返回支付信息，然后用支付信息请求app
+     * 第三方支付的sdk，然后支付成功，支付结果返回app，然后三方服务端将支付结果返回我们
+     * 自己的服务端，最后我们要调用本地服务器接口来确认支付成功。
+     *
+     *
+     * https://docs.open.alipay.com/59/103681/ （ali文档）
+     * https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=1417751808&token=&lang=zh_CN
+     * 微信文档
+     *
+     *
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ViewApp/src/main/java/com/liyafeng/view/viewpager/BasePagerAdapter.java
@@ -0,0 +1,58 @@
+package com.liyafeng.view.viewpager;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qusukj.baoguan.util.AppUtil;
+
+import java.util.List;
+
+public abstract class BasePagerAdapter<T> extends PagerAdapter {
+
+
+    List<T> list;
+    protected LayoutInflater inflater;
+
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+
+    public BasePagerAdapter(Context context, List<T> list) {
+        this(context);
+        this.list = list;
+    }
+
+
+    public BasePagerAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return list == null ? 0 : list.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        View view = getView(position);
+        container.addView(view);
+        return view;
+    }
+
+    public abstract View getView(int position);
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+}
