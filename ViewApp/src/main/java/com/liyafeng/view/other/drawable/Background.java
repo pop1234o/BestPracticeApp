637b317d9a2e70a38dd0639644ff4ab80e421ac0
pop1234o <859405648@qq.com>
2018-07-19 11:03:45 +0800

    添加了水波纹的代码

---
+++ ViewApp/src/main/java/com/liyafeng/view/other/drawable/Background.java
@@ -0,0 +1,24 @@
+package com.liyafeng.view.other.drawable;
+
+public class Background {
+
+
+    /**
+     * 很多时候我们需要设置点击效果的背景变化，我们可以用selector来实现
+     * 但是Android5.0自带水波纹效果，我们可以直接拿来用
+     * <p>
+     * android:background="?android:attr/selectableItemBackground"
+     * textview imagebutton都可以设置背景
+     * <p>
+     * 无界
+     * android:background="?android:attr/selectableItemBackgroundBorderless"
+     * <p>
+     * 有时候我们的控件需要自定义背景颜色，那么就可以自定义水波纹颜色
+     * https://blog.csdn.net/u012702547/article/details/52325418
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ViewApp/src/main/java/com/liyafeng/view/other/share/Main.java
@@ -0,0 +1,17 @@
+package com.liyafeng.view.other.share;
+
+public class Main {
+
+    /**
+     * 三方分享，微信好友，朋友圈，新浪微博，QQ好友和QQ空间
+     *
+     * 微信分享 https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=open1419317340&token=&lang=zh_CN
+     * 分享分享图片到朋友圈，有的时候会很慢，是因为你直接传了bitmap过去，你应该传递图片的path
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
