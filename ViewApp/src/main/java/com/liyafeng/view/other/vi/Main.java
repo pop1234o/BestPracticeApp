842a619dee7ecdd2fef6025764bd5b83ab66c258
pop1234o <859405648@qq.com>
2018-07-30 11:42:47 +0800

    添加了vi的使用

---
+++ ViewApp/src/main/java/com/liyafeng/view/other/vi/Main.java
@@ -0,0 +1,34 @@
+package com.liyafeng.view.other.vi;
+
+public class Main {
+
+
+    /**
+     * 一个编辑器，用命令行操作
+     * <p>
+     * vi filename
+     * <p>
+     * 按a进入insert mode
+     * 按esc进入命令模式
+     * 输入":wq!"这样就将文本保存了
+     * <p>
+     * i    插入当前光标
+     * I   当前行首
+     * a   光标后插入
+     * A   当前行尾
+     * <p>
+     * <p>
+     * x 删除光标后的
+     * X 删除光标前的
+     * d 删除折行
+     * <p>
+     * w 写入
+     * q 退出
+     * q! 强制退出
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
