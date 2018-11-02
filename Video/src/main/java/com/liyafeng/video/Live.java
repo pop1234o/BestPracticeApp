01323a8d20d4ee0cf48f7db5fce1c1a944dd3587
pop1234o <859405648@qq.com>
2018-11-02 20:22:25 +0800

    添加了视频知识

---
+++ Video/src/main/java/com/liyafeng/video/Live.java
@@ -0,0 +1,53 @@
+package com.liyafeng.video;
+
+public class Live {
+
+
+    /**
+     * 推流 ，
+     * 点播，
+     * 直播，
+     * 短视频，
+     * 连麦（观众能看到别人1v1对话），
+     * 1v1音视频通话
+     * nvn视频聊天室（多人音视频通话） 腾讯云（最多8个？），
+     *          声网（最多17个），网易云信，阿里云（最多16个）
+     *          七牛云（不限）
+     * ======================直播========================
+     * 搭建一个rtmp媒体服务器；
+     * 推流端；
+     * 拉流端。
+     *
+     * ==================点播===================
+     * 频点播是二十世纪90年代在国外发展起来的，
+     * 英文称为“Video on Demand”，所以也称为“VOD”
+     * 就像现在的电视盒子都是点播，爱奇艺，优酷等看电影都是点播的方式
+     * 和电视的实时播放时有区别的。
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * 多对多视频（多人视频通话）
+     * https://cloud.tencent.com/document/product/454/12723（腾讯解决方案）
+     *  声网解决方案
+     * https://www.agora.io/cn/blog/%E5%9F%BA%E4%BA%8Ewebrtc%E6%8A%80%E6%9C%AF%E7%9A%84%E5%A4%9A%E4%BA%BA%E9%9F%B3%E8%A7%86%E9%A2%91%E8%A7%A3%E5%86%B3%E6%96%B9%E6%A1%88/
+     *
+     */
+    public void f1(){
+
+    }
+
+    /**
+     * 1对1语音视频通话
+     * http://www.52im.net/thread-1201-1-1.html（微信语音视频通话）
+     * http://www.52im.net/thread-1311-1-1.html(腾讯自研实时视频聊天框架)
+     *
+     *
+     */
+    public void f2(){}
+}
