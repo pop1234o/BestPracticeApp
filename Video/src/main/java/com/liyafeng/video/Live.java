ade69db8e9dae8403f498007d86d6c854f11fa32
pop1234o <859405648@qq.com>
2018-11-05 20:11:39 +0800

    添加了视频知识

--- Video/src/main/java/com/liyafeng/video/Live.java
+++ Video/src/main/java/com/liyafeng/video/Live.java
@@ -10,10 +10,16 @@ public class Live {
      * 短视频，
      * 连麦（观众能看到别人1v1对话），
      * 1v1音视频通话
-     * nvn视频聊天室（多人音视频通话） 腾讯云（最多8个？），
+     * nvn视频聊天室（多人音视频通话/音视频通信（Real-Time Communication））
+     *          腾讯云（最多8个？），
      *          声网（最多17个），网易云信，阿里云（最多16个）
      *          七牛云（不限）
+     * ==========================
+     * 任何视频从一端传到另一端播放，都需要编码-》传输/推流-》（转码/合并）-》拉流-》解码-》展示
+     * 所以编解码规则，传输协议用的是哪个很关键
      * ======================直播========================
+     *
+     * Real-Time Messaging Protocol，实时消息传输协议
      * 搭建一个rtmp媒体服务器；
      * 推流端；
      * 拉流端。
@@ -27,12 +33,10 @@ public class Live {
      * ==================实时视频聊天============
      * WebRTC，名称源自网页即时通信（英语：Web Real-Time Communication）的缩写
      * 是一个支持网页浏览器进行实时语音对话或视频对话的API。
-     * 它于2011年6月1日开源并在Google、Mozilla、Opera支持下被纳入万维网联盟的W3C推荐标准[2][3][4]。
-     * 2010年5月，Google以6820万美元收购VoIP软件开发商Global IP Solutions的GIPS引擎[17][18][19]，并改为名为“WebRTC”
-     * WebRTC使用GIPS引擎，实现了基于网页的视频会议，并支持722，PCM，ILBC，ISAC等编码，
-     * 同时使用谷歌自家的VP8影片解码器；同时支持RTP/SRTP传输等
-     * 2012年1月，谷歌已经把这款软件集成到Chrome浏览器中。
      *
+     * 直播是从rtmp拉流的过程，而互动连麦是 音视频通话，是rtc框架
+     * 而观众看连麦的过程是主播和连麦者的音视频数据在服务器合成为1道流后
+     * 推送到cdn流媒体服务器。
      * @param args
      */
     public static void main(String[] args) {
@@ -41,10 +45,6 @@ public class Live {
 
 
     /**
-     * 多对多视频（多人视频通话）
-     * https://cloud.tencent.com/document/product/454/12723（腾讯解决方案）
-     *  声网解决方案
-     * https://www.agora.io/cn/blog/%E5%9F%BA%E4%BA%8Ewebrtc%E6%8A%80%E6%9C%AF%E7%9A%84%E5%A4%9A%E4%BA%BA%E9%9F%B3%E8%A7%86%E9%A2%91%E8%A7%A3%E5%86%B3%E6%96%B9%E6%A1%88/
      *
      */
     public void f1(){
--- Video/src/main/java/com/liyafeng/video/QT.java
+++ Video/src/main/java/com/liyafeng/video/QT.java
@@ -2,8 +2,21 @@ package com.liyafeng.video;
 
 public class QT {
     /**
+     * Cross-platform software development for embedded & desktop
+     * https://www.qt.io/
+     *
+     * 传统的在windows mac 上开发一款软件，需要些两套代码
+     * 而用qt则只需要一次编写
+     *
      * Qt是一个跨平台的C++图形用户界面应用程序框架，
-     * 也就是说你只要写一次代码就可以在不同的平台上运行，如Windows、Mac、Linux等。
+     * 也就是说你只要写一次代码就可以在不同的平台上运行，如Windows、Mac、Linux，Android iOS等。
+     *
+     * 二、Qt的历史
+     * 1991年，也是我出生的那一年由奇趣科技开发
+     * 1996年，进入商业领域
+     * 2008年，奇趣科技公司被若基亚公司收购
+     * 2012年，Qt又被Digia公司收购
+     *
      * @param args
      */
     public static void main(String[] args) {
