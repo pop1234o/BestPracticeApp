d5f520f91d0dcb3cddea0beee67b228c7887162a
pop1234o <859405648@qq.com>
2018-11-29 10:45:14 +0800

    添加了Linux知识

---
+++ ViewApp/src/main/java/com/liyafeng/view/other/linux/Linux_Cmd.java
@@ -0,0 +1,17 @@
+package com.liyafeng.view.other.linux;
+
+public class Linux_Cmd {
+
+
+    /**
+     * mv命令 ，文件移动
+     * mv /文件名  /目录
+     * 目标不能写文件名
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
