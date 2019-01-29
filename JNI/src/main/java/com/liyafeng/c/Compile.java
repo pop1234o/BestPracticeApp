f8a3a1610a763c961024fd3714f360481673501b
pop1234o <859405648@qq.com>
2019-01-29 21:48:20 +0800

    feat:添加了汇编的知识

---
+++ JNI/src/main/java/com/liyafeng/c/Compile.java
@@ -0,0 +1,32 @@
+package com.liyafeng.c;
+
+public class Compile {
+
+
+    /**
+     *      * ======================MinGW==========================
+     *      * https://mingw-w64.org/doku.php/download 下载
+     *      * https://sourceforge.net/projects/mingw-w64/
+     *      * <p>
+     *      * 在windows中要编译c语音，需要特定编译器
+     *      * mingw Minimalist GNU for Windows
+     *      * <p>
+     *   * ========================gcc===================
+     *      * 下面是GNU计划中开发的几个软件
+     *      * Emacs 一款c编辑器
+     *      * GNU C Compiler(gcc) GNU上的c语音编译器
+     *      * GNU C Library (glibc)
+     *      * Bash shell
+     *      * --------------------------------
+     *      * GNU编译器套装（英语：GNU Compiler Collection，缩写为GCC）
+     *      * 因为它原本只能处理C语言。GCC很快地扩展，变得可处理C++。之后也变得可处理Fortran、Pascal)、Objective-C、Java、Ada，以及Go与其他语言。
+     *      * 许多操作系统，包括许多类Unix系统，如Linux及BSD家族都采用GCC作为标准编译器。苹果电脑Mac OS X操作系统也采用这个编译器。
+     *      * <p>
+     *      * <p>
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
--- JNI/src/main/java/com/liyafeng/c/Operating_System.java
+++ JNI/src/main/java/com/liyafeng/c/Operating_System.java
@@ -3,9 +3,11 @@ package com.liyafeng.c;
 public class Operating_System {
 
     /**
-     * ### C语言
-     * =====================计算机历史=======================
-     * <p>
+     *
+     * =====================Unix历史======================
+     * https://bbs.feng.com/read-htm-tid-6209622-page-1.html
+     *
+     *
      * ================汇编语言 assembly language===============================
      * 控制硬件，是低级语言，直接控制（描述）cpu的运行
      * <p>
@@ -14,13 +16,7 @@ public class Operating_System {
      * 所以就有了汇编语言，他采用 操作符 操作数 的方式来编写
      * 经过编译器编译后变为机器语言执行。
      * 像unix，mysql都是c语言编写的，他的速度和汇编语言差不多。
-     * ======================MinGW==========================
-     * https://mingw-w64.org/doku.php/download 下载
-     * https://sourceforge.net/projects/mingw-w64/
-     * <p>
-     * 在windows中要编译c语音，需要特定编译器
-     * mingw Minimalist GNU for Windows
-     * <p>
+
      * ======================Linux/GNU================
      * http://cn.linux.vbird.org/linux_basic/0110whatislinux_1.php
      * <p>
@@ -48,18 +44,6 @@ public class Operating_System {
      * 并且同时增加了很多工具软件与编译程序，最终将它命名为Berkeley Software Distribution (BSD)。
      * 后来可以安装在x86硬件架构上面FreeBSD即是BSD改版而来
      * <p>
-     * <p>
-     * <p>
-     * ========================gcc===================
-     * 下面是GNU计划中开发的几个软件
-     * Emacs 一款c编辑器
-     * GNU C Compiler(gcc) GNU上的c语音编译器
-     * GNU C Library (glibc)
-     * Bash shell
-     * --------------------------------
-     * GNU编译器套装（英语：GNU Compiler Collection，缩写为GCC）
-     * 因为它原本只能处理C语言。GCC很快地扩展，变得可处理C++。之后也变得可处理Fortran、Pascal)、Objective-C、Java、Ada，以及Go与其他语言。
-     * 许多操作系统，包括许多类Unix系统，如Linux及BSD家族都采用GCC作为标准编译器。苹果电脑Mac OS X操作系统也采用这个编译器。
      * <p>
      * <p>
      * ==============Visual Studio===================
