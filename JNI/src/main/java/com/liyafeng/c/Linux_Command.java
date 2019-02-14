40a65b4a92b3d637f0c1f4cceba562b30b4d9236
pop1234o <859405648@qq.com>
2019-02-14 19:59:11 +0800

    feat:添加了linux命令

---
+++ JNI/src/main/java/com/liyafeng/c/Linux_Command.java
@@ -0,0 +1,24 @@
+package com.liyafeng.c;
+
+public class Linux_Command {
+
+
+    /**
+     * =====ps=======
+     * ps --help
+     * 显示进程列表
+     *
+     * ps -ef
+     * =======netstat============
+     * 查看网络状况
+     *
+     * netstat -nap  | grep 进程号
+     *
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
