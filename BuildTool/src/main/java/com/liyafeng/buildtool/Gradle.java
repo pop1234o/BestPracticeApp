1952d4c1c955843652f765013181d1f6466d9102
pop1234o <859405648@qq.com>
2019-02-15 14:29:35 +0800

    feat:添加了gradle配置

---
+++ BuildTool/src/main/java/com/liyafeng/buildtool/Gradle.java
@@ -0,0 +1,27 @@
+package com.liyafeng.buildtool;
+
+public class Gradle {
+    /**
+     * 2017 年google 后，Android studio 版本更新至3.0，更新中，
+     * 、连带着com.android.tools.build:gradle 工具也升级到了3.0.0，
+     * 在3.0.0中使用了最新的Gralde 4.0 里程碑版本作为gradle 的编译版本，
+     * 该版本gradle编译速度有所加速，更加欣喜的是，完全支持Java8。当然，对于Kotlin的支持，
+     * 在这个版本也有所体现，Kotlin插件默认是安装的。
+     * ---------------------
+     * 作者：迦蓝叶
+     * 来源：CSDN
+     * 原文：https://blog.csdn.net/soslinken/article/details/73114637
+     * 版权声明：本文为博主原创文章，转载请附上博文链接！
+     *
+     *
+     * =========api和implement区别==========
+     * api和implement区别
+     * api完全等于compile，implement依赖的包只能在本模块中引用到
+     * 而api可以
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
