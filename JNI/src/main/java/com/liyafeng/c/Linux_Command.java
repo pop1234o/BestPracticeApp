d5e91901d59fb4a8468e1322dae839f198235250
pop1234o <859405648@qq.com>
2019-02-20 20:57:25 +0800

    feat:添加了知识

--- JNI/src/main/java/com/liyafeng/c/Linux_Command.java
+++ JNI/src/main/java/com/liyafeng/c/Linux_Command.java
@@ -4,6 +4,11 @@ public class Linux_Command {
 
 
     /**
+     *
+     * linux命令大全
+     * http://man.linuxde.net
+     *
+     *
      * =====ps=======
      * ps --help
      * 显示进程列表
@@ -23,12 +28,42 @@ public class Linux_Command {
      * ==========sudo=========
      * sudo 用管理员的权限运行
      *
+     * =========wget==========
+     * http://man.linuxde.net/wget
+     * https://www.gnu.org/software/wget/
+     * 从指定的url下载文件
      *
+     * ==========source========
+     * source命令也称为“点命令”，也就是一个点符号（.）,是bash的内部命令。
+     * 功能：使Shell读入指定的Shell程序文件并依次执行文件中的所有语句
+     * source命令通常用于重新执行刚修改的初始化文件，使之立即生效，而不必注销并重新登录。
      *
      *
      * @param args
      */
     public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * linux shell
+     * http://wiki.jikexueyuan.com/project/shell-tutorial/shell-brief-introduction.html
+     *
+     *
+     *
+     */
+    public void shell(){}
+
+
+    /**
+     * linux教程
+     *
+     * http://c.biancheng.net/cpp/linux/
+     *
+     *
+     */
+    public void linux(){
 
     }
 }
--- ViewApp/src/main/java/com/liyafeng/view/other/adb_command.java
+++ ViewApp/src/main/java/com/liyafeng/view/other/adb_command.java
@@ -11,6 +11,11 @@ public class adb_command {
      *
      *
      *
+     *
+     * ===============查看arr======
+     * 其实想要查看AAR文件里面的内容很简单，只需要将文件名的后缀由".aar" 改为".zip", 然后再解压zip文件即可。
+     *
+     *
      * @param args
      */
     public static void main(String[] args) {
