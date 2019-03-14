4add5a52d1806332aab4f3e62c15831f0d1bc3e0
pop1234o <859405648@qq.com>
2019-03-14 16:08:18 +0800

    feat:添加了适配方案

--- Practicelib/src/main/java/com/liyafeng/practice/util/Util.java
+++ Practicelib/src/main/java/com/liyafeng/practice/util/Util.java
@@ -660,6 +660,16 @@ public class Util {
     }
 
 
+    /**
+     * 获取状态栏高度
+     * @return
+     */
+    public static int getStatusBarHeight() {
+        Resources resources = Resources.getSystem();
+        int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
+        return resources.getDimensionPixelSize(resourceId);
+    }
+
 
 
 
--- ViewApp/src/main/java/com/liyafeng/view/match/MatchActivity.java
+++ ViewApp/src/main/java/com/liyafeng/view/match/MatchActivity.java
@@ -1,14 +1,17 @@
 package com.liyafeng.view.match;
 
 import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 import com.liyafeng.view.R;
 
 public class MatchActivity extends Activity {
 
     /**
-     *
      * dip        ： Density independent pixels ，设备无关像素。
      * dp        ：就是dip
      * px        ： 像素
@@ -17,11 +20,11 @@ public class MatchActivity extends Activity {
      * 分辨率   ： 横纵2个方向的像素点的数量，常见取值 480X800 ，320X480
      * 屏幕尺寸： 屏幕对角线的长度。1英寸=2.54cm
      * 电脑电视同理。屏幕比例的问题。因为只确定了对角线长，2边长度还不一定。所以有了4：3、16：9这种，这样就可以算出屏幕边长了。
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * =================屏幕适配========================
-     *
+     * <p>
      * https://developer.android.google.cn/training/multiscreen/screendensities.html
      * https://www.jianshu.com/p/ec5a1a30694b （适配方案）
      * https://www.jianshu.com/p/220404575079 （Android设备兼容处理（二）：屏幕适配）
@@ -61,10 +64,10 @@ public class MatchActivity extends Activity {
      * <p>
      * ==========为什么标准dpi是160dpi （160像素/英寸）======
      * 1 （1）Android Design [1] 里把主流设备的 dpi 归成了四个档次，120 dpi、160 dpi、240 dpi、320 dpi
-     *  160 好换算
+     * 160 好换算
      * 2.（2）这个在Google的官方文档中有给出了解释，因为第一款Android设备（HTC的T-Mobile G1）是属于160dpi的。
-     *
-     *
+     * <p>
+     * <p>
      * ==========================mipmap===================
      * https://developer.android.google.cn/guide/topics/resources/providing-resources.html（官方的目录介绍）
      * mipmap在图片缩放上会有优化，但是在同等dpi的设备上，图片不缩放，那么和drawable是一样的效果
@@ -84,8 +87,8 @@ public class MatchActivity extends Activity {
      * xhdpi (Extra-high density screen, 320 DPI)，其图标大小为 96 x 96 px。
      * xxhdpi（xx-high density screen, 480 DPI）,其图标大小为144 x 144 px。
      * xxxhdpi（xxx-high density screen, 640 DPI）,其图标大小为192 x 192 px。
-     *
-     *
+     * <p>
+     * <p>
      * ===============分辨率对应dpi=========
      * https://blog.csdn.net/u012741741/article/details/51075518
      * 宽×高(标准值)	240×320	320×480	480×800	720×1280	1080×1920	1440×2560
@@ -94,88 +97,88 @@ public class MatchActivity extends Activity {
      * 对应比例	    3	    4	    6	    8	        12	        16
      * 1DP=？PX	    0.75	1	    1.5	    2	        3	        4
      * sw(Smallest-width)   320dp   320dp   360dp       360dp       360dp
-     *
+     * <p>
      * context.getResources().getDisplayMetrics().density
      * 在160dpi的屏幕上，density = 1  dpi的缩放因子  320dpi就是 density = 2
-     *
+     * <p>
      * context.getResources().getDisplayMetrics().densityDpi
      * 每英寸的像素点数，屏幕密度的另一种表示。densityDpi = density * 160
-     *
-     *
+     * <p>
+     * <p>
      * dpi是个范围不是特定的数值，比如440dpi和500dpi都会划分到xxhdpi
-     *
+     * <p>
      * android中的 2k屏就是宽度像素为1440？？
-     *
+     * <p>
      * 720P是一种在逐行扫描下达到1280×720的分辨率的显示格式。
-     *
+     * <p>
      * 720P是美国电影电视工程师协会(SMPTE)制定的高等级高清数字电视的格式标准，
      * 有效显示格式为：1280×720.SMPTE
      * (美国电影电视工程协会)将数字高清信号数字电视扫描线的不同分为1080p、1080i、720P(i是interlace,隔行的意思，p是Progressive,逐行的意思)。
-     *
+     * <p>
      * 720指的是屏幕的宽度
-     *
+     * <p>
      * 由于构成数字图像的像素数量巨大，通常以K来表示210即1024，因此：1K=2^10=1024，2K=2^11=2048，4K=2^12=4096。
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * 一般手机的宽度不变，高度可以变，所以根据手机宽度能确定他是哪个 dpi
-     *
+     * <p>
      * =========RelativeLayout====
      * 如果里面的控件是 imageview或者textview,是warp_content 这个时候如果外部指定的高度小了
      * 里面会压缩
-     *
+     * <p>
      * =========layout适配===========================
      * res文件下资源文件夹的命名规则
-     *      * https://developer.android.google.cn/guide/topics/resources/providing-resources.html
-     *
+     * * https://developer.android.google.cn/guide/topics/resources/providing-resources.html
+     * <p>
      * res/layout-sw480dp的意思，sw的意思是smallest width最小宽度，
-     *  设备的 smallestWidth 是屏幕可用高度和宽度的最小尺寸（您也可以将其视为屏幕的“最小可能宽度”）
-     *  应用为多个资源目录提供不同的 smallestWidth 限定符值时，系统会使用最接近（但未超出）设备 smallestWidth 的值。
-     *
-     *
+     * 设备的 smallestWidth 是屏幕可用高度和宽度的最小尺寸（您也可以将其视为屏幕的“最小可能宽度”）
+     * 应用为多个资源目录提供不同的 smallestWidth 限定符值时，系统会使用最接近（但未超出）设备 smallestWidth 的值。
+     * <p>
+     * <p>
      * 5.0英寸1080P（1080x1920）的手机屏幕宽度就是1080/(440/160) = 392dp
-     *
+     * <p>
      * layout-sw480dp-land/
-     *
-     *
+     * <p>
+     * <p>
      * 限定符命名规则:
      * 您可以为单组资源指定多个限定符，并使用短划线分隔。例如，drawable-en-rUS-land 适用于横排美国英语设备
      * 这些限定符必须遵循表 2 中列出的顺序。例如：
      * 错误：drawable-hdpi-port/
      * 正确：drawable-port-hdpi/
-     *
-     *
+     * <p>
+     * <p>
      * 将备用资源保存到以这些限定符命名的目录中之后，Android 会根据当前设备配置在应用中自动应用这些资源。
      * 每次请求资源时，Android 都会检查备用资源目录是否包含所请求的资源文件，然后查找最佳匹配资源（下文进行介绍）。
      * 如果没有与特定设备配置匹配的备用资源，则 Android 会使用相应的默认资源（一组用于不含配置限定符的特定资源类型的资源）
-     *
+     * <p>
      * large限定符是 Android3.2 以前的版本
      * sw 限定符是 3.2后的版本
      * a. 版本低于 3.2 的平板会匹配 large的文件
      * b. 版本高于 3.2 的平板会匹配 sw600dp的文件
-     *
+     * <p>
      * =============平板适配===============
      * 如下文，很显然，平板的smallest-width 都是600dp以上
      * 而手机的sw是320dp或者360dp
      * 所以我们能根据这个来确定哪个是平板，哪个是手机
-     *
-     *
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
+     * <p>
+     * <p>
      * =========1080像素===============
      * https://www.zhihu.com/question/20440405
-     *
+     * <p>
      * 比如有 1080*1920 （4.95英寸）的手机，和vivo nex 是 1080*2316（6.59英寸）
-     *
+     * <p>
      * 横屏情况下，他们的高度是一样的，只不过（4.95英寸）的手机屏幕窄，只是显示的内容少了
      * 如果是基于 alignParentRight 的布局那么也会相应缩短，（如果是横向listview那么只是显示内容少了）
      * 如果宽高给指定数值都是100dp，那么这个方块在两款手机上的大小是一样的
      * 但这样，横向占据的屏幕比例就不一样了
-     *
+     * <p>
      * 16/9=1.77777
      * 4/3 = 1.33333
-     *
+     * <p>
      * Android主流的手机 都是1080p，只不过长度会长，
      * 华为p20 ：2244x1080 5.8英寸,
      * vivo nex 2316*1080 6.59英寸
@@ -187,12 +190,12 @@ public class MatchActivity extends Activity {
      * 魅族16th 2160x1080 6英寸,
      * 小米Max3 2160x1080 6.9英寸
      * 红米Note7 2340x1080 6.3英寸
-     *
+     * <p>
      * 华为P30        2880x1440 6英寸, 2:1
      * 华为Mate20Pro  3120×1440 6.39英寸
      * 三星Note9      2960x1440  6.4英寸
      * 三星Galaxy S10  3040x1440 6.1英寸,
-     *
+     * <p>
      * =========平板 ===============
      * 华为的平板就三个尺寸
      * 1280x800（HDPI）    这个对应的手机的是 480×800  缩放因子1.3312501
@@ -200,71 +203,126 @@ public class MatchActivity extends Activity {
      * 2560x1600（XXHDPI） 这个对应的手机的是 1080×1920  缩放因子3.0
      * 这个屏幕扩大的1.333倍 ，而缩放因子却扩大了1.5倍，所以同样的10dp，在2560x1600的屏幕上显示要大1.125倍
      * （如果这两个分辨率的屏幕在物理上一样大）
-     *
+     * <p>
      * 所以 1920x1200 的中点位置是 1920/2=960px =480dp
-     *     2560x1600 的中点是    2560/2 = 1280px = 426.66dp
-     *
+     * 2560x1600 的中点是    2560/2 = 1280px = 426.66dp
+     * <p>
      * 480dp/426.66dp = 1.125 ( 3/2/(4/3)=1.125 )
-     *
+     * <p>
      * 这三个的比例一致都是1.6
      * 1920x1200 => 2560x1600 大了 4/3=1.33 倍 而缩放因子大了1.5倍
-     *
+     * <p>
      * 1280x800 => 1920x1200 大了 1.5倍 ，而缩放因子大了 1.50倍
-     *
+     * <p>
      * 所以10dp宽度在 1280x800  1920x1200 大小是一样的，所以只需要一套dp即可
-     *
+     * <p>
      * 1920x1200 => 2560x1600 在前者的10dp,后者就需要写 10dp/1.125 = 8.8888dp
      * 所以在这两个屏幕上 一个写10dp 一个写8.88dp ，两者在屏幕上的占比是一样的
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * ==================主流平板=================
      * 主流的平板都是上面的尺寸
      * 还有             1024*768 2048x1536 这种尺寸就是模仿 ipad  4/3 = 1.33
      * 三星、神州都有这种尺寸的平板
-     *
+     * <p>
      * 所以Android pad会长点
-     *
+     * <p>
      * 华为平板 M5 青春版
      * 1920x1200 10.1英寸
-     *
+     * <p>
      * 华为平板 M5 8.4英寸
      * 2560x1600
-     *
+     * <p>
      * 荣耀畅玩平板2（9.6英寸）
      * 1280x800
-     *
-     *
+     * <p>
+     * <p>
      * mi pad 4   8英寸
      * 1920*1200
-     *
-     *
+     * <p>
+     * <p>
      * 小米平板1-3 7.9英寸 （停产）
      * 2048x1536
-     *
-     *
+     * <p>
+     * <p>
      * 三星Galaxy Tab S5e 10.5英寸
      * 2560x1600
-     *
-     *
+     * <p>
+     * <p>
      * 华硕ZenPad 3S 10（Z500M/64GB） 9.7英寸
      * 2048x1536
-     *
-     *
+     * <p>
+     * <p>
      * ==========2018.1月市售手机分辨率=============
      * 安卓：
      * 2560 x 1440
      * 1920 x 1080
      * 1280 x 720
-     *
-     *
+     * ==========适配===============
+     * 适配目的，就是所有分辨率变为宽度一样，然后宽向的10dp的大小是一样的这样达到目的
+     * <p>
+     * <p>
+     * 不管什么分辨率的手机，我们都可以将宽度缩放到 720 来看
+     * 这时手机宽度一样，只是宽高比例不一样
+     * 所以我们只需要360dp的ui图（比如 720*1280 1080*1920 1440*2560）
+     * 然后换算成dp即可，10dp 在这三个手机上的大小是一样的（如果这三个手机尺寸一样）
+     * <p>
+     * 现在手机长度都长，所以我们还是可以根据dp，只不过长短不一样
+     * <p>
+     * <p>
+     * <p>
+     * <p>
      * =========适配总结================
      * 使用"wrap_content"、"match_parent"和"weight“来控制视图组件的宽度和高度
      * 使用ConstantLayout 适配
-     *
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
+     * ==========头条适配方案============
+     * 1920*1080，屏幕尺寸为5吋的话，那么dpi为440
+     * <p>
+     * 假设我们UI设计图是按屏幕宽度为360dp来设计的，那么在上述设备上，屏幕宽度其实为1080/(440/160)=392.7dp，
+     * 也就是屏幕是比设计图要宽的。这种情况下， 即使使用dp也是无法在不同设备上显示为同样效果的。 同时还存在部分设备屏幕宽度不足360dp，
+     * 这时就会导致按360dp宽度来开发实际显示不全的情况。
+     * 而且上述屏幕尺寸、分辨率和像素密度的关系，很多设备并没有按此规则来实现， 因此dpi的值非常乱，没有规律可循，从而导致使用dp适配效果差强人意。
+     * <p>
+     * 总结下大致需求如下：
+     * <p>
+     * 支持以宽或者高一个维度去适配，保持该维度上和设计图一致；
+     * 支持dp和sp单位，控制迁移成本到最小。
+     * <p>
+     * <p>
+     * 找兼容突破口
+     * <p>
+     * 从dp和px的转换公式 ：px = dp * density
+     * <p>
+     * 可以看出，如果设计图宽为360dp，想要保证在所有设备计算得出的px值都正好是屏幕宽度的话，我们只能修改 density 的值。
+     * <p>
+     * <p>
+     * <p>
+     * 通过阅读源码，我们可以得知，density 是 DisplayMetrics 中的成员变量，
+     * 而 DisplayMetrics 实例通过 Resources#getDisplayMetrics 可以获得，
+     * 而Resouces通过Activity或者Application的Context获得。
+     * <p>
+     * <p>
+     * 先来熟悉下 DisplayMetrics 中和适配相关的几个变量：
+     * DisplayMetrics#density 就是上述的density
+     * DisplayMetrics#densityDpi 就是上述的dpi
+     * DisplayMetrics#scaledDensity 字体的缩放因子，正常情况下和density相等，但是调节系统字体大小后会改变这个值
+     * 所以 dp是 value* density   sp = value* scaledDensity
+     * 因为系统能调节字体大小，所以才有了sp
+     * <p>
+     * <p>
+     * 首先来看看布局文件中dp的转换，最终都是调用 TypedValue#applyDimension(int unit, float value, DisplayMetrics metrics) 来进行转换
+     * <p>
+     * 再看看图片的decode，BitmapFactory#decodeResourceStream方法:可见也是通过 DisplayMetrics 中的值来计算的。
+     * <p>
+     * <p>
+     * 下面假设设计图宽度是360dp，以宽维度来适配。
+     * 那么适配后的 density = 设备真实宽(单位px) / 360，接下来只需要把我们计算好的 density 在系统中修改下即可，代码实现如下：
+     * <p>
+     * 比如 720*1280为基准，他的density=2   有个1080的设备 ，那就是density=(1080/720)*2 =1080/360=3
      *
      * @param savedInstanceState
      */
@@ -273,4 +331,53 @@ public class MatchActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
     }
+
+
+    /**
+     * 这个在baseActivity的onCreate中调用即可
+     */
+    public static class ScreenUtil{
+        public static float sNoncompatDensity;//原始的Density
+        public static float sNoncompatScaledDensity;
+
+        public static void setCustomDensity(Activity activity, final Application application) {
+            DisplayMetrics displayMetrics = application.getResources().getDisplayMetrics();
+            if (sNoncompatDensity == 0) {
+                sNoncompatDensity = displayMetrics.density;
+                sNoncompatScaledDensity = displayMetrics.scaledDensity;
+                application.registerComponentCallbacks(new ComponentCallbacks() {
+
+                    @Override
+                    public void onConfigurationChanged(Configuration newConfig) {
+                        if (newConfig != null && newConfig.fontScale > 0) {
+                            sNoncompatScaledDensity = application.getResources().getDisplayMetrics().scaledDensity;
+                        }
+                    }
+
+                    @Override
+                    public void onLowMemory() {
+
+                    }
+                });
+            }
+
+
+            float targetDensity = displayMetrics.widthPixels / 360.0f;
+            float targetScaledDensity = targetDensity * (sNoncompatScaledDensity / sNoncompatDensity);
+            int targetDensityDpi = (int) (160 * targetDensity);
+
+            displayMetrics.density = targetDensity;
+            displayMetrics.scaledDensity = targetScaledDensity;
+            displayMetrics.densityDpi = targetDensityDpi;
+
+            DisplayMetrics activityDisplayMetrics = activity.getResources().getDisplayMetrics();
+
+            activityDisplayMetrics.density = targetDensity;
+            activityDisplayMetrics.scaledDensity = targetScaledDensity;
+            activityDisplayMetrics.densityDpi = targetDensityDpi;
+
+
+        }
+    }
+
 }
