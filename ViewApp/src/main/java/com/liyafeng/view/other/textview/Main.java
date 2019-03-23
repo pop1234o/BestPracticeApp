2e0d884144d308fa5ab979ac160d51059355bce9
pop1234o <859405648@qq.com>
2019-03-23 15:00:04 +0800

    feat:添加了textview

---
+++ ViewApp/src/main/java/com/liyafeng/view/other/textview/Main.java
@@ -0,0 +1,37 @@
+package com.liyafeng.view.other.textview;
+
+public class Main {
+
+    /**
+     * https://www.cnblogs.com/tianzhijiexian/p/4222393.html
+     * http://www.jcodecraeer.com/a/anzhuokaifa/androidkaifa/2015/0120/2335.html
+     *
+     * Spannable
+     *
+     * SpannableString msp = new SpannableString("测试文字");
+     *
+     * 设置字体样式（TypefaceSpan）
+     * 大小      AbsoluteSizeSpan(20)
+     * 相对大小  RelativeSizeSpan(0.5f)
+     *
+     * ForegroundColorSpan
+     * BackgroundColorSpan
+     * StyleSpan
+     * UnderlineSpan
+     * StrikethroughSpan
+     * SubscriptSpan 下标和上标
+     * SuperscriptSpan
+     * URLSpan 跳转url
+     * ScaleXSpan
+     * IconMarginSpan
+     * ImageSpan
+     * MaskFilterSpan
+     *
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
