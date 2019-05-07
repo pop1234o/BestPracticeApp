4cfef12e2cb604ef775fbf8640f869226a575064
pop1234o <859405648@qq.com>
2019-05-07 17:06:37 +0800

    feat:添加了mvc mvp mvvm区别

--- Architecture/src/main/java/com/liyafeng/architecture/Main.java
+++ Architecture/src/main/java/com/liyafeng/architecture/Main.java
@@ -7,40 +7,37 @@ package com.liyafeng.architecture;
 public class Main {
 
     /**
-     * @param args
-     * 基于rxjava的MVP
-     * https://dev.qq.com/topic/57bfef673c1174283d60bac0
-     *
-     * -------------
-     * view (activity ,fragment) 数据展示
-     * -------------
-     * presenter (+rxjava) 业务逻辑
-     * -------------
-     * repository   数据获取/处理/缓存
-     * -------------
-     * network cache database 原始数据的获取，存储
-     *
-     *
+     * @param args 基于rxjava的MVP
+     *             https://dev.qq.com/topic/57bfef673c1174283d60bac0
+     *             <p>
+     *             -------------
+     *             view (activity ,fragment) 数据展示
+     *             -------------
+     *             presenter (+rxjava) 业务逻辑
+     *             -------------
+     *             repository   数据获取/处理/缓存
+     *             -------------
+     *             network cache database 原始数据的获取，存储
      */
     public static void main(String[] args) {
 
     }
+
     /**
      * google  https://github.com/googlesamples/android-architecture
+     * 这是个mvp的示例
      *
      *
-    *
-    * */
-    void a1(){}
+     */
+    void a1() {
+    }
 
 
     /**
      * 设计思想
      * =======配置文件==========
      * 很多软件把很多参数写入配置文件，这样增加了软件的灵活性，比如ss的配置文件
-     *
-     *
-     *
      */
-    void a2(){}
+    void a2() {
+    }
 }
--- Architecture/src/main/java/com/liyafeng/architecture/mvp/Main_MVP.java
+++ Architecture/src/main/java/com/liyafeng/architecture/mvp/Main_MVP.java
@@ -4,13 +4,21 @@ package com.liyafeng.architecture.mvp;
  * Created by liyafeng on 2018/1/9.
  */
 
-public class Main {
+public class Main_MVP {
     /**
      * @param args
      * mvp真正实现了业务逻辑和 UI逻辑的解耦
      * 与UI无关的都在presenter中，业务表现层
      * UI显示的都用接口方式回调
      * ----------------
+     * 这里的 activity implement view，activity就相当于view，
+     * 而mvc中的 activity相当于controller
+     *
+     *
+     * =========与mvc的区别===========
+     * 以前是数据在model中直接返回给view进行更新，
+     * 而现在数据先返回到presenter中，然后由presenter持有的view进行更新
+     *
      *
      *
      */
