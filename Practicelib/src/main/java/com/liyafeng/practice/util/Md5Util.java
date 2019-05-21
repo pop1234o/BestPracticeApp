7ccfcb862daf1d1227afccb0206185d5f62e23f0
pop1234o <859405648@qq.com>
2019-05-21 18:58:04 +0800

    feat:添加了工具类

--- Practicelib/src/main/java/com/liyafeng/practice/util/Md5Util.java
+++ Practicelib/src/main/java/com/liyafeng/practice/util/Md5Util.java
@@ -83,4 +83,27 @@ public class Md5Util {
         return reStr;
 
     }
+
+
+    public static String getMessageDigest(byte[] buffer) {
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+        try {
+            MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+            mdTemp.update(buffer);
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (byte byte0 : md) {
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+
+
 }
