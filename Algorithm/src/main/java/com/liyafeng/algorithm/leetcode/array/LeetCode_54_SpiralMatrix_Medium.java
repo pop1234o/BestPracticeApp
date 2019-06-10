6c03b8c5fe0fd9039b7495b455b6307e652c6563
pop1234o <859405648@qq.com>
2019-06-10 16:51:26 +0800

    feat:添加了LeetCode

--- Algorithm/src/main/java/com/liyafeng/algorithm/leetcode/array/LeetCode_54_SpiralMatrix_Medium.java
+++ Algorithm/src/main/java/com/liyafeng/algorithm/leetcode/array/LeetCode_54_SpiralMatrix_Medium.java
@@ -21,7 +21,14 @@ public class LeetCode_54_SpiralMatrix_Medium {
      * ]
      * Output: [1,2,3,6,9,8,7,4,5]
      * <p>
+     * [
+     * [1,2,3,4],
+     * [5,6,7,8],
+     * [9,10,11,12]
+     * ]
      * =========思路==============
+     * 指针变化+举例分析法
+     * 控制四个指针进行边界控制
      * <p>
      * <p>
      * <p>
@@ -40,10 +47,49 @@ public class LeetCode_54_SpiralMatrix_Medium {
 
     public static List<Integer> spiralOrder(int[][] matrix) {
         LinkedList<Integer> integers = new LinkedList<>();
-        int rowBegin = 0, rowEnd = matrix.length;
-        int colBegin = 0, colEnd = matrix[0].length;
+        if (matrix == null) {
+            return integers;
+        }
+        if (matrix.length == 0) {
+            return integers;
+        }
+        int rowBegin = 0, rowEnd = matrix.length - 1;
+        int colBegin = 0, colEnd = matrix[0].length - 1;
 
-        while (rowBegin<=rowEnd&&colBegin<=colEnd){
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            //第一行
+            for (int i = colBegin; i <= colEnd; i++) {
+                integers.add(matrix[rowBegin][i]);
+            }
+
+            rowBegin++;
+
+            //最后一列
+            for (int i = rowBegin; i <= rowEnd; i++) {
+                integers.add(matrix[i][colEnd]);
+            }
+
+            colEnd--;
+
+            //这里判断是为了 3行4列这种情况
+            if (rowBegin <= rowEnd) {
+                //最后一行
+                for (int i = colEnd; i >= colBegin; i--) {
+                    integers.add(matrix[rowEnd][i]);
+                }
+            }
+
+
+            rowEnd--;
+
+            if (colBegin <= colEnd) {
+                //第一列
+                for (int i = rowEnd; i >= rowBegin; i--) {
+                    integers.add(matrix[i][colBegin]);
+                }
+            }
+
+            colBegin++;
 
         }
 
