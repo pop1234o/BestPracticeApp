6610935fd02d6cf749d722d75362421099702259
pop1234o <859405648@qq.com>
2019-07-16 11:36:06 +0800

    feat:添加了工具类

---
+++ Practicelib/src/main/java/com/liyafeng/practice/util/ToastUtil.java
@@ -0,0 +1,33 @@
+package com.tal.lib_common.utils;
+
+import android.support.annotation.IdRes;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tal.lib_common.R;
+import com.tal.lib_common.app.BaseApplication;
+
+public class ToastUtil {
+
+
+    private static void showToast(String content, int resId) {
+        BaseApplication context = BaseApplication.getIns();
+        Toast toast = new Toast(context);
+        View view = LayoutInflater.from(context).inflate(R.layout.common_toast, null);
+        ImageView ivToastIcon = view.findViewById(R.id.ivToastIcon);
+        TextView tvToastText = view.findViewById(R.id.tvToastText);
+
+        ivToastIcon.setImageResource(resId);
+        tvToastText.setText(content);
+        toast.setView(view);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+}
