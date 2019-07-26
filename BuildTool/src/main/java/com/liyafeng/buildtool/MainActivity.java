28ff6c006e75e1a7373a72f8e898c5afa40d25a1
pop1234o <859405648@qq.com>
2019-07-26 14:33:07 +0800

    feat:添加了知识

--- BuildTool/src/main/java/com/liyafeng/buildtool/MainActivity.java
+++ BuildTool/src/main/java/com/liyafeng/buildtool/MainActivity.java
@@ -317,4 +317,41 @@ public class MainActivity extends Activity {
      */
     void a3() {
     }
+
+
+    /**
+     * Android 7.0	24	N	平台亮点
+     * Android 6.0	23	M	平台亮点
+     * Android 5.1	22	LOLLIPOP_MR1	平台亮点
+     * Android 5.0	21	LOLLIPOP
+     * Android 4.4W	20	KITKAT_WATCH	仅限 KitKat for Wearables
+     * Android 4.4	19	KITKAT	平台亮点
+     * Android 4.3	18	JELLY_BEAN_MR2	平台亮点
+     * Android 4.2、4.2.2	17	JELLY_BEAN_MR1	平台亮点
+     * Android 4.1、4.1.1	16	JELLY_BEAN	平台亮点
+     * Android 4.0.3、4.0.4	15	ICE_CREAM_SANDWICH_MR1	平台亮点
+     * Android 4.0、4.0.1、4.0.2	14	ICE_CREAM_SANDWICH
+     * Android 3.2	13	HONEYCOMB_MR2	 
+     * Android 3.1.x	12	HONEYCOMB_MR1	平台亮点
+     * Android 3.0.x	11	HONEYCOMB	平台亮点
+     * Android 2.3.4
+     * Android 2.3.3	10	GINGERBREAD_MR1	平台亮点
+     * Android 2.3.2
+     * Android 2.3.1
+     * Android 2.3	9	GINGERBREAD
+     * Android 2.2.x	8	FROYO	平台亮点
+     * Android 2.1.x	7	ECLAIR_MR1	平台亮点
+     * Android 2.0.1	6	ECLAIR_0_1
+     * Android 2.0	5	ECLAIR
+     * Android 1.6	4	DONUT	平台亮点
+     * Android 1.5	3	CUPCAKE	平台亮点
+     * Android 1.1	2	BASE_1_1	 
+     * Android 1.0	1	BASE	 
+     * ---------------------
+     * 作者：HawkJony
+     * 来源：CSDN
+     * 原文：https://blog.csdn.net/qiaoquan3/article/details/70185550
+     * 版权声明：本文为博主原创文章，转载请附上博文链接！
+     */
+    void a4(){}
 }
