5a34dd0e80a57ef16bacaf17dbc35478230ad86f
pop1234o <859405648@qq.com>
2019-09-19 19:41:08 +0800

    feat:添加了知识

--- Event/src/main/java/com/liyafeng/event/rxjava/RxJavaSample.java
+++ Event/src/main/java/com/liyafeng/event/rxjava/RxJavaSample.java
@@ -101,6 +101,33 @@ public class RxJavaSample {
         do9();
     }
 
+    //region 线程切换
+
+    /**
+     * rxjava线程切换
+     * =========subscribeOn 和 observeOn 的区别=====
+     * https://segmentfault.com/a/1190000004856071（ subscribeOn 和 observeOn 的区别）
+     *
+     * subscribeOn的调用切换之前的线程。
+     * observeOn的调用切换之后的线程。
+     * observeOn之后，不可再调用subscribeOn 切换线程
+     *
+     * 只有第一subscribeOn() 起作用（所以多个 subscribeOn() 毛意义）
+     * 这个 subscribeOn() 控制从流程开始的第一个操作，直到遇到第一个 observeOn()
+     *
+     * observeOn() 可以使用多次，每个 observeOn() 将导致一次线程切换()，这次切换开始于这次 observeOn() 的下一个操作
+     *
+     * 不论是 subscribeOn() 还是 observeOn()，后面"操作"线程将不再改变，不会自动切换到其他线程
+     * 直到受到下一个 observeOn() 的干预
+     *
+     */
+    static void d10(){
+
+
+    }
+
+    //endregion
+
     //region 创建操作
 
 
