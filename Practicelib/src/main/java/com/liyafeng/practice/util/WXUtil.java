4670e59423cd8e5d82ad874d1138eadaad19a5a7
pop1234o <859405648@qq.com>
2019-11-04 17:03:31 +0800

    feat:添加了知识

--- Practicelib/src/main/java/com/liyafeng/practice/util/WXUtil.java
+++ Practicelib/src/main/java/com/liyafeng/practice/util/WXUtil.java
@@ -1,5 +1,6 @@
 package com.liyafeng.practice.util;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
@@ -170,6 +171,88 @@ public class WXUtil {
 
             Log.i(TAG, "bitmap required size=" + newWidth + "x" + newHeight + ", orig=" + options.outWidth + "x" + options.outHeight + ", sample=" + options.inSampleSize);
             Bitmap bm = BitmapFactory.decodeFile(path, options);
+            if (bm == null) {
+                Log.e(TAG, "bitmap decode failed");
+                return null;
+            }
+
+            Log.i(TAG, "bitmap decoded size=" + bm.getWidth() + "x" + bm.getHeight());
+            final Bitmap scale = Bitmap.createScaledBitmap(bm, newWidth, newHeight, true);
+            if (scale != null) {
+                bm.recycle();
+                bm = scale;
+            }
+
+            if (crop) {
+                final Bitmap cropped = Bitmap.createBitmap(bm, (bm.getWidth() - width) >> 1, (bm.getHeight() - height) >> 1, width, height);
+                if (cropped == null) {
+                    return bm;
+                }
+
+                bm.recycle();
+                bm = cropped;
+                Log.i(TAG, "bitmap croped size=" + bm.getWidth() + "x" + bm.getHeight());
+            }
+            return bm;
+
+        } catch (final OutOfMemoryError e) {
+            Log.e(TAG, "decode bitmap failed: " + e.getMessage());
+            options = null;
+        }
+
+        return null;
+    }
+
+
+//    private static final int MAX_DECODE_PICTURE_SIZE = 65536;
+
+    public static Bitmap extractThumbNail(Resources resources, final int resId, final int height, final int width, final boolean crop) {
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        try {
+            options.inJustDecodeBounds = true;
+
+            Bitmap tmp = BitmapFactory.decodeResource(resources, resId, options);
+            if (tmp != null) {
+                tmp.recycle();
+                tmp = null;
+            }
+
+            Log.d(TAG, "extractThumbNail: round=" + width + "x" + height + ", crop=" + crop);
+            final double beY = options.outHeight * 1.0 / height;
+            final double beX = options.outWidth * 1.0 / width;
+            Log.d(TAG, "extractThumbNail: extract beX = " + beX + ", beY = " + beY);
+            options.inSampleSize = (int) (crop ? (beY > beX ? beX : beY) : (beY < beX ? beX : beY));
+            if (options.inSampleSize <= 1) {
+                options.inSampleSize = 1;
+            }
+
+            // NOTE: out of memory error
+            while (options.outHeight * options.outWidth / options.inSampleSize > MAX_DECODE_PICTURE_SIZE) {
+                options.inSampleSize++;
+            }
+
+            int newHeight = height;
+            int newWidth = width;
+            if (crop) {
+                if (beY > beX) {
+                    newHeight = (int) (newWidth * 1.0 * options.outHeight / options.outWidth);
+                } else {
+                    newWidth = (int) (newHeight * 1.0 * options.outWidth / options.outHeight);
+                }
+            } else {
+                if (beY < beX) {
+                    newHeight = (int) (newWidth * 1.0 * options.outHeight / options.outWidth);
+                } else {
+                    newWidth = (int) (newHeight * 1.0 * options.outWidth / options.outHeight);
+                }
+            }
+
+            options.inJustDecodeBounds = false;
+
+            Log.i(TAG, "bitmap required size=" + newWidth + "x" + newHeight + ", orig=" + options.outWidth + "x" + options.outHeight + ", sample=" + options.inSampleSize);
+            Bitmap bm = BitmapFactory.decodeResource(resources, resId, options);
             if (bm == null) {
                 Log.e(TAG, "bitmap decode failed");
                 return null;
