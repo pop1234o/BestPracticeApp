069cd25173442633ac6be813008f62b99437730d
pop <859405648@qq.com>
2019-11-20 23:31:48 +0800

    feat:添加了知识

---
+++ Video/src/main/java/com/liyafeng/video/FFmpeg.java
@@ -0,0 +1,21 @@
+package com.liyafeng.video;
+
+public class FFmpeg {
+
+
+    /**
+     * https://www.ffmpeg.org/
+     * A complete, cross-platform solution to record, convert and stream audio and video
+     * 一个完整的，跨平台的录制和转换音视频的解决方案
+     * 其实就是一个软件
+     * 比如从视频提取声音
+     * ffmpeg -i input.mp4 output.avi
+     *
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
