a6b9fecd0b52f48aa99d2c792b81c4a15f450a8f
pop1234o <859405648@qq.com>
2019-12-25 18:48:12 +0800

    feat:添加了知识

--- Practicelib/src/main/java/com/liyafeng/practice/util/ToastUtil.java
+++ Practicelib/src/main/java/com/liyafeng/practice/util/ToastUtil.java
@@ -12,19 +12,19 @@ public class ToastUtil {
 
 
     private static void showToast(String content, int resId) {
-        BaseApplication context = BaseApplication.getIns();
-        Toast toast = new Toast(context);
-        View view = LayoutInflater.from(context).inflate(R.layout.common_toast, null);
-        ImageView ivToastIcon = view.findViewById(R.id.ivToastIcon);
-        TextView tvToastText = view.findViewById(R.id.tvToastText);
-
-        ivToastIcon.setImageResource(resId);
-        tvToastText.setText(content);
-        toast.setView(view);
-        toast.setGravity(Gravity.CENTER, 0, 0);
-
-        toast.setDuration(Toast.LENGTH_SHORT);
-        toast.show();
+//        BaseApplication context = BaseApplication.getIns();
+//        Toast toast = new Toast(context);
+//        View view = LayoutInflater.from(context).inflate(R.layout.common_toast, null);
+//        ImageView ivToastIcon = view.findViewById(R.id.ivToastIcon);
+//        TextView tvToastText = view.findViewById(R.id.tvToastText);
+//
+//        ivToastIcon.setImageResource(resId);
+//        tvToastText.setText(content);
+//        toast.setView(view);
+//        toast.setGravity(Gravity.CENTER, 0, 0);
+//
+//        toast.setDuration(Toast.LENGTH_SHORT);
+//        toast.show();
     }
 
 }
