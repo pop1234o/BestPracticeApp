64bb043c91a887aacc371b3d1049a2d699c2062e
pop1234o <859405648@qq.com>
2019-12-27 20:19:02 +0800

    feat:添加了知识

--- JNI/src/main/java/com/liyafeng/c/Compile.java
+++ JNI/src/main/java/com/liyafeng/c/Compile.java
@@ -4,42 +4,13 @@ public class Compile {
 
 
     /**
-     *      * ======================MinGW==========================
-     *      * https://mingw-w64.org/doku.php/download 下载
-     *      * https://sourceforge.net/projects/mingw-w64/
-     *      * <p>
-     *      * 在windows中要编译c语音，需要特定编译器
-     *      * mingw Minimalist GNU for Windows
-     *      * <p>
-     *   * ========================gcc===================
-     *      * 下面是GNU计划中开发的几个软件
-     *      * Emacs 一款c编辑器
-     *      * GNU C Compiler(gcc) GNU上的c语音编译器
-     *      * GNU C Library (glibc)
-     *      * Bash shell
-     *      * --------------------------------
-     *      * GNU编译器套装（英语：GNU Compiler Collection，缩写为GCC）
-     *      * 因为它原本只能处理C语言。GCC很快地扩展，变得可处理C++。之后也变得可处理Fortran、Pascal)、Objective-C、Java、Ada，以及Go与其他语言。
-     *      * 许多操作系统，包括许多类Unix系统，如Linux及BSD家族都采用GCC作为标准编译器。苹果电脑Mac OS X操作系统也采用这个编译器。
-     *      * <p>
-     *      * <p>
-     *
-     *  =====================LLVM=========================
-     *  C++写成的编译器 底层虚拟机（Low Level Virtual Machine）
-     *
-     * 现今LLVM已单纯成为一个品牌，适用于LLVM下的所有项目，包含LLVM中介码（LLVM IR）、LLVM调试工具、LLVM C++标准库等。
-     *
-     * LLVM项目的发展起源于2000年伊利诺伊大学厄巴纳-香槟分校维克拉姆·艾夫（Vikram Adve）
-     * 与克里斯·拉特纳（Chris Lattner）的研究，他们想要为所有静态及动态语言创造出动态的编译技术。
-     * LLVM是以BSD许可来发展的开源软件。
-     * 2005年，苹果计算机雇用了克里斯·拉特纳及他的团队为苹果计算机开发应用程序系统[3]，
-     * LLVM为现今Mac OS X及iOS开发工具的一部分。
-     *
-     * =================静态编译和动态编译=====================
-     * 静态编译是将所有代码编译到一个可执行文件中
-     * 动态编译是生成一个可执行文件作为入口，其他的生成动态链接库
-     * 运行的时候加载这个库。
-     *
+     *  ======================MinGW==========================
+     *  https://mingw-w64.org/doku.php/download 下载
+     *  https://sourceforge.net/projects/mingw-w64/
+     *  <p>
+     *  在windows中要编译c语音，需要特定编译器
+     *  mingw Minimalist GNU for Windows
+     *   <p>
      *
      *
      * @param args
@@ -73,6 +44,16 @@ public class Compile {
      * ============ 链接器 Linker============
      * http://blog.jobbole.com/96225/ (帮 C/C++ 程序员彻底了解链接器)
      * https://juejin.im/post/5cd8042151882568d154c4bb （链接器到底是如何工作的？）
+     * 见drawable/link.jpg
+     *
+     * hello.c-预处理器>hello.i -编译器> hello.s -汇编器-> hello.o (可重定位目标文件) -链接器> hello.exe
+     *
+     * ===============可执行文件==============
+     * mac和linux的可执行程序的扩展名可以是任意的，也可以没有扩展名，但是都被文件系统标志为可执行
+     * （权限一般755 wrx-rx-rx 或者777 wrxwrxwrx，x表示可执行，参考chmod命令），图标是一个命令行窗口的样子
+     *
+     * 不过一般的应用程序是一个扩展名为.app的文件夹（权限也是一般755 wrx-rx-rx 或者777 wrxwrxwrx，其实很多文件夹都是这个权限），
+     * 它的资源文件和可执行程序都在这个文件夹里面，当你双击这个文件夹的时候系统就知道你要打开这个程序（
      *
      *
      * =============宏=============
@@ -107,4 +88,205 @@ public class Compile {
      *
      */
     public void tools(){}
+
+
+
+    /**
+     * ====================gcc（GNU Compiler Collection）介绍==========
+     * https://zh.wikipedia.org/wiki/GCC
+     * GNU编译器套装（英语：GNU Compiler Collection，缩写为GCC），指一套编程语言编译器，以GPL及LGPL许可证所发行的自由软件，
+     * 也是GNU计划的关键部分，也是GNU工具链的主要组成部分之一
+     * GCC（特别是其中的C语言编译器）也常被认为是跨平台编译器的事实标准。
+     * 原名为GNU C语言编译器（GNU C Compiler），因为它原本只能处理C语言。GCC在发布后很快地得到扩展，变得可处理C++。
+     * 之后也变得可处理Fortran、Pascal、Objective-C、Java、Ada，Go与其他语言。
+     *
+     * 开始只能编译c后来支持c++ oc等语言的编译，成为一个工具集
+     *
+     * =============教程========
+     * http://c.biancheng.net/gcc/ （GCC编译器（Linux gcc命令）30分钟入门教程）
+     *
+     *
+     * ==================================
+     * mac 自带编译软件，gcc/g++
+     * gcc --version
+     * g++ --help
+     * <p>
+     * =========gcc/g++区别========
+     * 后缀为.c的，gcc把它当作是C程序，而g++当作是c++程序；后缀为.cpp的，两者都会认为是c++程序
+     * 编译阶段，g++会调用gcc，对于c++代码，两者是等价的，
+     * 但是因为gcc命令不能自动和C＋＋程序使用的库联接，所以通常用g++来完成链接，为了统一起见，干脆编译/链接统统用g++了
+     *
+     *  ========================gcc===================
+     * 下面是GNU计划中开发的几个软件
+     * Emacs 一款c编辑器
+     * GNU C Compiler(gcc) GNU上的c语音编译器
+     * GNU C Library (glibc)
+     * Bash shell
+     * --------------------------------
+     * GNU编译器套装（英语：GNU Compiler Collection，缩写为GCC）
+     * 因为它原本只能处理C语言。GCC很快地扩展，变得可处理C++。之后也变得可处理Fortran、Pascal)、Objective-C、Java、Ada，以及Go与其他语言。
+     * 许多操作系统，包括许多类Unix系统，如Linux及BSD家族都采用GCC作为标准编译器。苹果电脑Mac OS X操作系统也采用这个编译器。
+     * <p>
+     * <p>
+     * ================工作步骤==============
+     * https://www.runoob.com/w3cnote/gcc-parameter-detail.html （GCC 参数详解）
+     * gcc 与 g++ 分别是 gnu 的 c & c++ 编译器 gcc/g++ 在执行编译工作的时候，总共需要4步：
+     *
+     * 1、预处理,生成 .i 的文件[预处理器cpp]
+     * 2、将预处理后的文件转换成汇编语言, 生成文件 .s [编译器egcs]
+     * 3、有汇编变为目标代码(机器代码)生成 .o 的文件[汇编器as]
+     * 4、连接目标代码, 生成可执行程序 [链接器ld]
+     *
+     *
+     * https://www.cnblogs.com/qytan36/archive/2010/05/25/1743955.html (GCC编程四个过程:预处理-编译-汇编-链接)
+     * gcc可以实现这四个步骤
+     *
+     * 链接
+     * gcc hello.o –o hello.exe
+     *
+     * =================静态编译和动态编译=====================
+     *  静态编译是将所有代码编译到一个可执行文件中，因此生成的文件比较大
+     *  动态编译是生成一个可执行文件作为入口，其他的生成动态链接库
+     *  运行的时候加载这个库。
+     *
+     * 在没有特别指定时，gcc会到系统默认的搜索路径”/usr/lib”下进行查找依赖的库
+     * libc.so.6的库文件是系统中自带的
+     * .a是静态库的后缀
+     * .so是动态库的后缀
+     *
+     *
+     *
+     * ================gcc语法===================
+     * https://blog.csdn.net/tomatofly/article/details/6035363 （简单的GCC语法： 弄清gcc test.c 与 gcc -c test.c 的差别）
+     *
+     * gcc –c test.c，表示只编译test.c文件，成功时输出目标文件test.o
+     *
+     * gcc –c test.c –o test.o ，与上一条命令完全相同
+     *
+     * gcc –o test test.o，将test.o连接成可执行的二进制文件test
+     *
+     * gcc –o test test.c，将test.c编译并连接成可执行的二进制文件test
+     *
+     * gcc test.c –o test，与上一条命令相同
+     *
+     * gcc –c test1.c，只编译test1.c，成功时输出目标文件test1.o
+     *
+     * gcc –c test2.c，只编译test2.c，成功时输出目标文件test2.o
+     *
+     * gcc –o test test1.o test2.o，将test1.o和test2.o连接为可执行的二进制文件test
+     *
+     * gcc –c test test1.c test2.c，将test1.o和test2.o编译并连接为可执行的二进制文件test
+     *
+     * 注：如果你想编译cpp文件，那么请用g++，否则会有类似如下莫名其妙的错误：
+     *
+     * cc3r3i2U.o(.eh_frame+0x12): undefined reference to `__gxx_personality_v0’......
+     *
+     */
+    public void fun1() {
+    }
+
+
+    /**
+     *   =====================LLVM=========================
+     *   C++写成的编译器 底层虚拟机（Low Level Virtual Machine）
+     *
+     *  现今LLVM已单纯成为一个品牌，适用于LLVM下的所有项目，包含LLVM中介码（LLVM IR）、LLVM调试工具、LLVM C++标准库等。
+     *
+     *  LLVM项目的发展起源于2000年伊利诺伊大学厄巴纳-香槟分校维克拉姆·艾夫（Vikram Adve）
+     *  与克里斯·拉特纳（Chris Lattner）的研究，他们想要为所有静态及动态语言创造出动态的编译技术。
+     *  LLVM是以BSD许可来发展的开源软件。
+     *  2005年，苹果计算机雇用了克里斯·拉特纳及他的团队为苹果计算机开发应用程序系统[3]，
+     *  LLVM为现今Mac OS X及iOS开发工具的一部分。
+     *
+     *
+     *
+     *  =======================Clang===============
+     *
+     */
+    void fun2(){}
+
+
+    /**
+     * ==============make==================
+     * https://www.gnu.org/software/make/ （官方文档）
+     * https://www.gnu.org/software/make/manual/make.html （官方文档）
+     * https://zhuanlan.zhihu.com/p/72616078 （浅谈 GNU Make 构建项目实践）
+     * Make是最常用的构建工具，诞生于1977年，主要用于C语言的项目。
+     *
+     * make依赖gcc
+     *
+     *
+     * 工程大了，源文件就多了
+     * 如此多的源文件，如果每次都要键入gcc命令进行编译的话，那对程序员 来说简直就是一场灾难。
+     * 而make工具则可自动完成编译工作，并且可以只对程序员在上次编译后修改过的部分进行编译
+     *
+     * ==========用法============
+     * https://wangchujiang.com/linux-command/c/make.html (make用法)
+     *
+     *
+     *
+     *
+     *
+     */
+    void fun_make(){}
+
+
+
+    /**
+     * ==========Android studio中支持cmake和 lldb =======
+     * 打开 设置，里面搜索 Android Sdk ，里面有sdk tools ，勾选 cmake和lldb即可
+     * 下载的文件在sdk目录下，[sdkdir]/camke/版本/   [sdkdir]/lldb/版本
+     * <p>
+     * <p>
+     * <p>
+     * <p>
+     * =================cmake========================
+     * CMake is an open-source, cross-platform family of tools designed to build, test and package software.
+     * 开源，跨平台的构建，测试，打包工具
+     *
+     *
+     * ===================教材===========
+     * https://cmake.org/ （官网）
+     * https://cmake.org/cmake/help/latest/guide/tutorial/index.html （官方教材）
+     * https://juejin.im/post/5a6f32e86fb9a01ca6031230 （中文翻译）
+     *
+     * ===============================
+     *
+     * https://www.jianshu.com/p/6332418b12b1 （Android NDK开发扫盲及最新CMake的编译使用）
+     * 跨平台编译工具
+     * CMake的配置文件一般命名为CMakeLists.txt
+     * -------cmake和ndk-build区别---------
+     * 在Android Studio 2.2 之后，工具中增加了 CMake 的支持
+     * 用 cmake 替换 ndk-build +Application.mk+Android.mk 来编译 C/C++
+     * <p>
+     * 在 Android Studio 2.2 之后你有2种选择来编译你写的 c/c++ 代码。
+     * 一个是 ndk-build + Android.mk + Application.mk 组合，
+     * 另一个是 CMake + CMakeLists.txt 组合。
+     * 这2个组合与Android代码和c/c++代码无关，只是不同的构建脚本和构建命令。
+     * <p>
+     * cmake优点：
+     * 不需要再去通过javah根据java文件生成头文件，并根据头文件生成的函数声明编写cpp文件
+     * 当在Java文件中定义完native接口，可以在cpp文件中自动生成对应的native函数，所需要做的只是补全函数体中的内容
+     * 不需要手动执行ndk-build命令得到so，再将so拷贝到对应的目录
+     * 在编写cpp文件的过程中，可以有提示了
+     * CMakeLists.txt要比Android.mk更加容易理解
+     * <p>
+     * ---------------cmakeList.txt脚本语法-------
+     * https://cmake.org/cmake/help/latest/manual/cmake-commands.7.html
+     * <p>
+     * <p>
+     * <p>
+     * ----------cmake原理--------
+     * c/c++ 的编译文件在不同平台是不一样的。Unix 下会使用 makefile 文件编译，
+     * Windows 下会使用 project 文件编译。而 CMake 则是一个跨平台的编译工具，
+     * 它并不会直接编译出对象，而是根据自定义的语言规则（CMakeLists.txt）
+     * 生成 对应 makefile 或 project 文件，然后再调用底层的编译。
+     * <p>
+     * --------------使用命令行进行cmake编译---------------
+     * https://blog.csdn.net/minghuang2017/article/details/78938852
+     *
+     *
+     *
+     */
+    void fun_cmake(){}
 }
