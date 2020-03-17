099236a05417ab2e8d254b0a05f0f0de8a5ba407
pop <859405648@qq.com>
2020-03-17 22:02:25 +0800

    feat:添加了知识

---
+++ ViewApp/src/main/java/com/liyafeng/view/other/Anim.java
@@ -0,0 +1,30 @@
+package com.liyafeng.view.other;
+
+public class Anim {
+
+    /**
+     * ======android动画============
+     * https://developer.android.google.cn/training/animation/overview
+     *
+     * ========属性动画========
+     * https://developer.android.google.cn/guide/topics/graphics/prop-animation
+     * ValueAnimator
+     * ObjectAnimator
+     * 使用 AnimatorSet 编排多个动画
+     * PropertyValuesHolder
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+//        ObjectAnimator animation = ObjectAnimator.ofFloat(textView, "translationX", 100f);
+//        animation.setDuration(1000);
+//        animation.start();
+
+//        PropertyValuesHolder pvhX = PropertyValuesHolder.ofFloat("x", 50f);
+//        PropertyValuesHolder pvhY = PropertyValuesHolder.ofFloat("y", 100f);
+//        ObjectAnimator.ofPropertyValuesHolder(myView, pvhX, pvhY).start();
+
+
+    }
+}
