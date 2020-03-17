abd03f3c77f4e96880d4e38fccd4bb78ccbb3510
pop <859405648@qq.com>
2020-03-17 13:35:49 +0800

    feat:添加了知识

--- Network/src/main/java/com/liyafeng/network/Main.java
+++ Network/src/main/java/com/liyafeng/network/Main.java
@@ -41,6 +41,55 @@ public class Main {
      * 一个要30美元醉了
      *
      *
+     * =========Android 抓包配置==========
+     * https://www.jianshu.com/p/59e9ef771ff2 （Android 7.0 以上 Charles 和 Fiddler 无法抓取 HTTPS 包的解决方式）
+     *
+     * --------每个版本的默认配置
+     * Android 6.0（API 23）及更低版本应用的默认网络安全性配置如下：
+     * <!-- 默认允许所有明文通信 -->
+     * <base-config cleartextTrafficPermitted="true">
+     *     <trust-anchors>
+     *         <!-- 信任系统预装 CA 证书 -->
+     *         <certificates src="system" />
+     *         <!-- 信任用户添加的 CA 证书，Charles 和 Fiddler 抓包工具安装的证书属于此类 -->
+     *         <certificates src="user" />
+     *     </trust-anchors>
+     * </base-config>
+     *
+     * 而在 Android 7.0（API 24）到 Android 8.1（API 27）的默认网络安全性配置如下
+     * <!-- 默认允许所有明文通信 -->
+     * <base-config cleartextTrafficPermitted="true">
+     *     <trust-anchors>
+     *         <!-- 信任系统预装 CA 证书 -->
+     *         <certificates src="system" />
+     *     </trust-anchors>
+     * </base-config>
+     *
+     *  Android 9.0（API 28）及更高版本的默认网络安全性配置如下
+     *
+     *  <!-- 默认禁止所有明文通信 -->
+     * <base-config cleartextTrafficPermitted="false">
+     *     <trust-anchors>
+     *         <!-- 信任系统预装 CA 证书 -->
+     *         <certificates src="system" />
+     *     </trust-anchors>
+     * </base-config>
+     *
+     * 9.0默认禁止明文就是http协议，如果你设置了target api 是28+
+     *
+     * ===========解决方案=====
+     * <?xml version="1.0" encoding="utf-8"?>
+     * <network-security-config>
+     *     <base-config cleartextTrafficPermitted="true">
+     *          <debug-overrides>
+     *         <trust-anchors>
+     *             <certificates src="system" />
+     *             <certificates src="user" />
+     *         </trust-anchors>
+     *          <debug-overrides>
+     *     </base-config>
+     * </network-security-config>
+     *
      *
      */
     void a2(){}
